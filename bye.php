68f3fe703f9154acd8c60fb0ff65d51b9a72226e
Ubuntu <ubuntu@ip-172-31-25-109.us-west-2.compute.internal>
2015-08-26 02:39:30 +0000

    Adding http foundation

--- bye.php
+++ bye.php
@@ -1,5 +1,5 @@
 <?php
-// framework/index.php
+// framework/bye.php
 
 require_once __DIR__.'/vendor/autoload.php';
 
@@ -8,8 +8,5 @@ use Symfony\Component\HttpFoundation\Response;
 
 $request = Request::createFromGlobals();
 
-$input = $request->get('name', 'World');
-
-$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
-
+$response = new Response('Goodbye!');
 $response->send();
